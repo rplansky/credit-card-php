a3431ffbaf2ce6b321b45b62f708b62a798d5dcf
Ricardo Plansky <ricardo.plansky@yahoo.com>
2015-03-23 21:37:29 -0300

    Initial implementation of Validator class

---
+++ src/Plansky/CreditCard/Validator.php
@@ -0,0 +1,24 @@
+<?php
+namespace Plansky\CreditCard;
+
+/**
+* Validates credit card numbers using the Luhm algorithm. This validator does
+* not validates BIN and specific brand or bank alrogithms.
+*/
+class Validator
+{
+    /**
+     * Validates the given number using Luhm algorithm
+     *
+     * @param  string|integer $number
+     *
+     * @return boolean
+     */
+    public function isValid($number)
+    {
+        $lastDigit = substr($number, -1);
+        $number    = substr($number, 0, -1);
+
+        return (new LuhmCalculator())->verificationDigit($number) == $lastDigit;
+    }
+}
