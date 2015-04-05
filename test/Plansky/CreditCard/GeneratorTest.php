9d74625802f255df5ac2b99c1ac52b52aad18fee
Ricardo Plansky <ricardo.plansky@yahoo.com>
2015-04-05 17:31:54 -0300

    Added test cases for Generator and Validator classes

---
+++ test/Plansky/CreditCard/GeneratorTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Plansky\CreditCard;
+
+/**
+* Test case for Generator class
+*/
+class GeneratorTest extends TestCase
+{
+    public function testSingleShouldGenerateAValidRandomNumber()
+    {
+        $number = $this->generator->single();
+
+        $this->assertLength(16, $number);
+        $this->assertTrue($this->validator->isValid($number));
+    }
+
+    public function testSingleShouldGenerateAValidNumberWithGivenPrefix()
+    {
+        $this->assertPrefix('123', $this->generator->single(123));
+    }
+
+    public function testSingleShouldGenerateAValidNumberWithCustomLength()
+    {
+        $this->assertLength(10, $this->generator->single(null, 10));
+    }
+
+    public function testLotShouldGenerateAnArrayOfValidNumbers()
+    {
+        $numbers = $this->generator->lot(10);
+
+        foreach ($numbers as $number) {
+            $this->assertLength(16, $number);
+            $this->assertTrue($this->validator->isValid($number));
+        }
+    }
+
+    public function testLotShouldGenerateAnArrayOfNumbersWithGivenPrefix()
+    {
+        $numbers = $this->generator->lot(10, 123);
+
+        foreach ($numbers as $number) {
+            $this->assertPrefix('123', $number);
+        }
+    }
+
+    public function testLotShouldGenerateAnArrayOfValidNumbersWithCustomLength()
+    {
+        $numbers = $this->generator->lot(10, null, 10);
+
+        foreach ($numbers as $number) {
+            $this->assertLength(10, $number);
+        }
+    }
+}
---
+++ test/Plansky/CreditCard/TestCase.php
@@ -0,0 +1,80 @@
+<?php
+namespace Plansky\CreditCard;
+
+/**
+* Base test case for credit card tests
+*/
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Generator
+     */
+    protected $generator;
+
+    /**
+     * @var Validator
+     */
+    protected $validator;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->generator = new Generator();
+        $this->validator = new Validator();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+        unset($this->generator);
+        unset($this->validator);
+    }
+
+    /**
+     * Checks length of the given string
+     *
+     * @param integer $length
+     * @param string  $number
+     */
+    public function assertLength($length, $number)
+    {
+        $this->assertEquals($length, strlen($number));
+    }
+
+    /**
+     * Checks prefix of the given credit card number
+     *
+     * @param integer $prefix
+     * @param string  $number
+     */
+    public function assertPrefix($prefix, $number)
+    {
+        $this->assertEquals(0, strpos($this->generator->single(123)));
+    }
+
+    /**
+     * Checks if the given number is valid using Luhn Algorithm
+     *
+     * @param  integer $number
+     */
+    public function assertCreditCard($number)
+    {
+        $lastDigit   = substr($number, -1);
+        $numberArray = array_reverse(str_split($number));
+
+        $sum = 0;
+        for ($index = 0; $index < count($numberArray); $index++) {
+            $digit = (int)$numberArray[$index];
+            $result = $digit * 2;
+            $result = ($result >= 10) ? $result - 9 : $result;
+            $sum += ($index % 2 == 0) ? $result : $digit;
+        }
+
+        return $lastDigit == (10 - ($sum % 10 ?: 10));
+    }
+}
