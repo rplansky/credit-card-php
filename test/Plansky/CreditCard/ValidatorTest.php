ac1d862674d7b7a31a15e0ab1c8cd12fb7a53144
Ricardo Plansky <ricardo.plansky@yahoo.com>
2015-04-06 13:42:20 -0300

    Fixed test case of Validator.

--- test/Plansky/CreditCard/ValidatorTest.php
+++ test/Plansky/CreditCard/ValidatorTest.php
@@ -11,7 +11,7 @@ class ValidatorTest extends TestCase
      */
     public function testIsValidShouldValidatesNumberSuccessfully($number, $expected)
     {
-        $this->assertEquals($expected, $number);
+        $this->assertEquals($expected, $this->validator->isValid($number));
     }
 
     /**
@@ -32,7 +32,6 @@ class ValidatorTest extends TestCase
             [1939738871582291, true],
             [4851540879338546, true],
             [7630614502651988, true],
-            [0365529516550305, true],
             [5079130680956042, true],
             [5082644595250395, true],
             [1808712880876570, true],
@@ -40,16 +39,16 @@ class ValidatorTest extends TestCase
             [8548419608785345, true],
             [3452386767190945, true],
             [5790632602471008, true],
-            [5674392856342972, false],
+            [5674392856342973, false],
             [5478659746532453, false],
             [2543294548735347, false],
-            [3653654645643634, false],
+            [3653654645643632, false],
             [1111111111111111, false],
             [5674302389652843, false],
             [9374326542364373, false],
             [0173543743843642, false],
             [1538435485473433, false],
-            [5272937454172928, false],
+            [5272937454172925, false],
             [7032532823732326, false],
             [4358713621331562, false],
             [6381517653213173, false],
@@ -57,7 +56,7 @@ class ValidatorTest extends TestCase
             [4397482639449327, false],
             [0493747823649832, false],
             [4315487538423465, false],
-            [3271849637432432, false],
+            [3271849637432431, false],
         ];
     }
 }
